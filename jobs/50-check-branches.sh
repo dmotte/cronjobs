1affefe52b8ee88261918cbd28734a807920ff73
dmotte <37443982+dmotte@users.noreply.github.com>
2023-12-02 00:07:21 +0100

    Improved jobs/50-check-branches.sh

--- jobs/50-check-branches.sh
+++ jobs/50-check-branches.sh
@@ -7,12 +7,24 @@ cd "$(dirname "$0")"
 # shellcheck source=/dev/null
 . ../shared/fetch-and-check.sh
 
+script_git_status_all_branches="$(fetch_and_check \
+    'https://raw.githubusercontent.com/dmotte/misc/main/scripts/git-status-all-branches.sh' \
+    'bd1d5628f389f6d3cb556e7d6d83aac8cbc7455c8e5c16d37aaf1a511b3da811')"
+git_status_all_branches() { bash <(echo "$script_git_status_all_branches") "$@"; }
+
 script_git_check_branches="$(fetch_and_check \
     'https://raw.githubusercontent.com/dmotte/misc/main/scripts/git-check-branches.sh' \
     'a30788edfaf11588aabefedd386dbef23ec99c7e023c68896d108df5a4163ed0')"
 git_check_branches() { bash <(echo "$script_git_check_branches") "$@"; }
 
-# shellcheck disable=SC2046
-git_check_branches $(find repos -mindepth 1 -maxdepth 1)
+repolist="$(find repos -mindepth 1 -maxdepth 1)"
+
+# The git_status_all_branches step is a prerequisite because we need to get all
+# the branches as refs/heads/* instead of refs/remotes/*
+# shellcheck disable=SC2086
+git_status_all_branches $repolist
+
+# shellcheck disable=SC2086
+git_check_branches $repolist
 
 echo OK

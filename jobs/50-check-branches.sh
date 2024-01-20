be77dbf64d88e741d56cffacc3f4a4ce2cf72619
dmotte <37443982+dmotte@users.noreply.github.com>
2024-01-20 16:43:31 +0100

    Added bash explicit invocations for misc scripts

--- jobs/50-check-branches.sh
+++ jobs/50-check-branches.sh
@@ -9,9 +9,9 @@ repolist="$(find repos -mindepth 1 -maxdepth 1)"
 # The git_status_all_branches step is a prerequisite because we need to get all
 # the branches as refs/heads/* instead of refs/remotes/*
 # shellcheck disable=SC2086
-"$MISC_SCRIPTS_DIR/git-status-all-branches.sh" $repolist
+bash "$MISC_SCRIPTS_DIR/git-status-all-branches.sh" $repolist
 
 # shellcheck disable=SC2086
-"$MISC_SCRIPTS_DIR/git-check-branches.sh" $repolist
+bash "$MISC_SCRIPTS_DIR/git-check-branches.sh" $repolist
 
 echo OK

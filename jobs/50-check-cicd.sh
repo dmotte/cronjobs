7521ca6f6b2324f6a8a388d0fd8df649cc20f414
dmotte <37443982+dmotte@users.noreply.github.com>
2024-01-20 16:17:24 +0100

    Added 50-check-cicd.sh and improved 50-grep-tasks.sh a bit

---
+++ jobs/50-check-cicd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+check_grep() {
+    echo "Checking if pattern $1 can be found in file $2"
+    if ! grep "$1" "$2" >/dev/null; then
+        echo 'Pattern could not be found' >&2; exit 1
+    fi
+}
+
+find repos -mindepth 1 -maxdepth 1 | while read -r i; do
+    check_grep \
+        'uses: dmotte/misc/\.github/workflows/cicd-with-script\.yml@main' \
+        "$i/.github/workflows/main.yml"
+    check_grep 'package-ecosystem: github-actions' "$i/.github/dependabot.yml"
+done
+
+echo OK

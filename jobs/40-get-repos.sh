3628b75e0eb9a47143e2da2e98f9873850de02ad
dmotte <37443982+dmotte@users.noreply.github.com>
2024-01-20 16:40:49 +0100

    Added the MISC_SCRIPTS_DIR env var instead of fetch_and_check

--- jobs/40-get-repos.sh
+++ jobs/40-get-repos.sh
@@ -4,20 +4,12 @@ set -e
 
 cd "$(dirname "$0")"
 
-# shellcheck source=/dev/null
-. ../shared/fetch-and-check.sh
-
-script_github_get_all_repos="$(fetch_and_check \
-    'https://raw.githubusercontent.com/dmotte/misc/main/scripts/github-get-all-repos.sh' \
-    'd4a3c3a4b3c3c8877e51638bacae5aae010fc1df920887321ebc606f0918c583')"
-github_get_all_repos() { bash <(echo "$script_github_get_all_repos") "$@"; }
-
 mkdir repos
 
 (
     cd repos
 
-    github_get_all_repos users/dmotte \
+    "$MISC_SCRIPTS_DIR/github-get-all-repos.sh" users/dmotte \
         '.archived == false and .fork == false' | \
         tr -d '\r' | while read -r i; do
             git clone --depth=1 --no-single-branch "https://github.com/$i.git"
--- jobs/50-check-branches.sh
+++ jobs/50-check-branches.sh
@@ -4,27 +4,14 @@ set -e
 
 cd "$(dirname "$0")"
 
-# shellcheck source=/dev/null
-. ../shared/fetch-and-check.sh
-
-script_git_status_all_branches="$(fetch_and_check \
-    'https://raw.githubusercontent.com/dmotte/misc/main/scripts/git-status-all-branches.sh' \
-    'c60a8b94d0310f521b66426a7ebb8e3e982025b97ad6405da546d53aaca478ba')"
-git_status_all_branches() { bash <(echo "$script_git_status_all_branches") "$@"; }
-
-script_git_check_branches="$(fetch_and_check \
-    'https://raw.githubusercontent.com/dmotte/misc/main/scripts/git-check-branches.sh' \
-    'a30788edfaf11588aabefedd386dbef23ec99c7e023c68896d108df5a4163ed0')"
-git_check_branches() { bash <(echo "$script_git_check_branches") "$@"; }
-
 repolist="$(find repos -mindepth 1 -maxdepth 1)"
 
 # The git_status_all_branches step is a prerequisite because we need to get all
 # the branches as refs/heads/* instead of refs/remotes/*
 # shellcheck disable=SC2086
-git_status_all_branches $repolist
+"$MISC_SCRIPTS_DIR/git-status-all-branches.sh" $repolist
 
 # shellcheck disable=SC2086
-git_check_branches $repolist
+"$MISC_SCRIPTS_DIR/git-check-branches.sh" $repolist
 
 echo OK

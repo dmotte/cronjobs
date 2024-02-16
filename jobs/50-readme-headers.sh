9e557f6f42078c2713c6b19af319b60688286139
dmotte <37443982+dmotte@users.noreply.github.com>
2024-02-16 19:27:52 +0100

    Added verbosity to 50-readme-headers.sh

--- jobs/50-readme-headers.sh
+++ jobs/50-readme-headers.sh
@@ -5,6 +5,8 @@ set -e
 cd "$(dirname "$0")"
 
 find . -type f -iname README.md | while read -r i; do
+    echo "Checking $i"
+
     expected="# $(basename "$(dirname "$i")")"
     actual="$(head -1 "$i")"
 

c12776f2c3bb02c4b97389c61668fd3d307d8c81
dmotte <37443982+dmotte@users.noreply.github.com>
2025-06-23 00:05:57 +0200

    Added -F to 50-grep-tasks

--- jobs/50-grep-tasks.sh
+++ jobs/50-grep-tasks.sh
@@ -6,10 +6,10 @@ cd "$(dirname "$0")"
 
 echo 'Checking if any tasks are present in the repos'
 
-if find repos ! -name .git ! -path '*/.git/*' | grep -i TO'DO'; then
+if find repos ! -name .git ! -path '*/.git/*' | grep -Fi TO'DO'; then
     echo 'Pattern found in some paths' >&2; exit 1
 fi
 
-if grep -IRil --exclude-dir=.git TO'DO' repos; then
+if grep -FIRil --exclude-dir=.git TO'DO' repos; then
     echo 'Pattern found in some files' >&2; exit 1
 fi

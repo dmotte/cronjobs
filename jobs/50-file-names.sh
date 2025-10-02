d18344f9598d9638706575c3731995053f9b4545
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-02 16:23:03 +0200

    Job 50-file-names split pipeline

--- jobs/50-file-names.sh
+++ jobs/50-file-names.sh
@@ -6,7 +6,8 @@ cd "$(dirname "$0")"
 
 echo 'Checking file names'
 
-if find repos ! -name .git ! -path '*/.git/*' -printf '%f\n' |
-    grep '[^0-9A-Za-z./_-]'; then
+filenames=$(find repos ! -name .git ! -path '*/.git/*' -printf '%f\n')
+
+if echo "$filenames" | grep '[^0-9A-Za-z./_-]'; then
     echo 'Unwanted characters found in some file names' >&2; exit 1
 fi

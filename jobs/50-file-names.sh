89133d2b62e135e2d3b1ae1dddc43e81b10929ef
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-02 14:49:01 +0200

    Added job 50-file-names.sh

---
+++ jobs/50-file-names.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+echo 'Checking file names'
+
+if find repos ! -name .git ! -path '*/.git/*' | grep '[^0-9A-Za-z./_-]'; then
+    echo 'Unwanted characters found in some file names' >&2; exit 1
+fi

5d9d45cb467c5b70c041a314e57ea35f1e4499ed
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-02 14:22:20 +0200

    Simplified 40-get-repos.sh

--- jobs/40-get-repos.sh
+++ jobs/40-get-repos.sh
@@ -7,14 +7,5 @@ set -e
 
 cd "$(dirname "$0")"
 
-mkdir repos
-
-(
-    cd repos
-
-    bash "$MISC_SCRIPTS_DIR/github-get-all-repos.sh" users/dmotte \
-        '.archived == false and .fork == false' |
-        tr -d '\r' | while read -r i; do
-            git clone --depth=1 --no-single-branch "https://github.com/$i.git"
-        done
-)
+GHBAK_CLONE_ARGS='--depth=1 --no-single-branch' \
+    bash "$MISC_SCRIPTS_DIR/github-bak-all-repos.sh" users/dmotte repos

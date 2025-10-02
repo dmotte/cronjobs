16d4f85ca747cba1a9d5f3644744e7bd6f88182f
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-02 16:23:48 +0200

    Job 50-file-names removed slash

--- jobs/50-file-names.sh
+++ jobs/50-file-names.sh
@@ -8,6 +8,6 @@ echo 'Checking file names'
 
 filenames=$(find repos ! -name .git ! -path '*/.git/*' -printf '%f\n')
 
-if echo "$filenames" | grep '[^0-9A-Za-z./_-]'; then
+if echo "$filenames" | grep '[^0-9A-Za-z._-]'; then
     echo 'Unwanted characters found in some file names' >&2; exit 1
 fi

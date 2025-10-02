d3c245372a7b72def791179f3c03572b6267749b
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-02 15:06:08 +0200

    Job 50-code-style find in repos

--- jobs/50-code-style.sh
+++ jobs/50-code-style.sh
@@ -10,7 +10,7 @@ if grep -IRl --exclude-dir=.git '\s$' repos; then
     echo 'Trailing spaces found in some files' >&2; exit 1
 fi
 
-# find . -type f ! -path '*/.git/*' | while read -r i; do
+# find repos -type f ! -path '*/.git/*' | while read -r i; do
 #     [[ $(file -b --mime-type "$i") = text/* ]] || continue
 
 #     if [ "$(echo -n x; tail -c1 "$i"; echo x)" != $'x\nx' ]; then

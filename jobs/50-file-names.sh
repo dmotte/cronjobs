1f63e6535462a434ed2af1e474d0b57ad1414a89
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-02 14:56:41 +0200

    Job 50-file-names improved formatting

--- jobs/50-file-names.sh
+++ jobs/50-file-names.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")"
 
 echo 'Checking file names'
 
-if find repos ! -name .git ! -path '*/.git/*' -printf '%f\n' | grep '[^0-9A-Za-z./_-]'; then
+if find repos ! -name .git ! -path '*/.git/*' -printf '%f\n' |
+    grep '[^0-9A-Za-z./_-]'; then
     echo 'Unwanted characters found in some file names' >&2; exit 1
 fi

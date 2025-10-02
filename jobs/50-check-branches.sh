5ead51b768e74bddbcde32724ebc4c8d2802d35c
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-02 16:33:32 +0200

    Job 50-check-branches fixed comment about git-status-all-branches

--- jobs/50-check-branches.sh
+++ jobs/50-check-branches.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")"
 
 repolist=$(find repos -mindepth 1 -maxdepth 1)
 
-# The git_status_all_branches step is a prerequisite because we need to get all
+# Running git-status-all-branches is a prerequisite because we need to get all
 # the branches as refs/heads/* instead of refs/remotes/*
 echo -n "$repolist" | xargs -rd\\n \
     bash "$MISC_SCRIPTS_DIR/git-status-all-branches.sh"

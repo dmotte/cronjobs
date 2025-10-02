86a8ec8b3086cb7a12d6beabdfc4fa11432b9e4f
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-02 16:19:04 +0200

    Job 50-code-style fixed detection of files with no newline at the end

--- jobs/50-code-style.sh
+++ jobs/50-code-style.sh
@@ -10,10 +10,10 @@ if grep -IRl --exclude-dir=.git '\s$' repos; then
     echo 'Trailing spaces found in some files' >&2; exit 1
 fi
 
-# find repos -type f ! -path '*/.git/*' | while read -r i; do
-#     [[ $(file -b --mime-type "$i") = text/* ]] || continue
+files=$(find repos -type f ! -path '*/.git/*' ! -name '*.php' ! -name '*.svg')
+files_bad=$(echo -n "$files" | xargs -rd\\n grep -ILPz '\n\z' || :)
 
-#     if [ "$(echo -n x; tail -c1 "$i"; echo x)" != $'x\nx' ]; then
-#         echo "No newline at end of file: $i" >&2; exit 1
-#     fi
-# done
+[ -z "$files_bad" ] || {
+    echo "$files_bad"
+    echo 'Found some files with no newline at the end' >&2; exit 1
+}

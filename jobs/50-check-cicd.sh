493d03a506c870f2e17ab54dfb2ce4241e6597d8
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-02 15:01:24 +0200

    Job 50-check-cicd split pipeline

--- jobs/50-check-cicd.sh
+++ jobs/50-check-cicd.sh
@@ -5,12 +5,14 @@ set -e
 cd "$(dirname "$0")"
 
 check_grep() {
-    echo "Checking if pattern $1 can be found in file $2"
+    echo "Checking if pattern ${1@Q} can be found in file $2"
     grep "$1" "$2" >/dev/null ||
         { echo 'Pattern could not be found' >&2; return 1; }
 }
 
-find repos -mindepth 1 -maxdepth 1 | while read -r i; do
+repolist=$(find repos -mindepth 1 -maxdepth 1)
+
+echo "$repolist" | while read -r i; do
     check_grep \
         'uses: dmotte/misc/\.github/workflows/cicd-with-script\.yml@main' \
         "$i/.github/workflows/main.yml"

5c7b8450bfcd369888fc4c04a7b44701e943a735
dmotte <37443982+dmotte@users.noreply.github.com>
2025-11-17 17:50:00 +0100

    Job 50-git-modes

---
+++ jobs/50-git-modes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Ensure that some variables are defined
+: "${MISC_SCRIPTS_DIR:?}"
+
+cd "$(dirname "$0")"
+
+echo 'Checking that there are no git files with unexpected mode bits'
+
+repolist=$(find repos -mindepth 1 -maxdepth 1)
+
+echo "$repolist" | while read -r i; do
+    echo "Checking repo $i"
+    bash "$MISC_SCRIPTS_DIR/cicd/check-git-modes.sh" "$i"
+done
